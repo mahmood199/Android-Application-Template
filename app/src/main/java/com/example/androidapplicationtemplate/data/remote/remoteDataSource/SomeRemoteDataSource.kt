bd7fda74ef2b6b8b029b5f9fc99cb9dc38666f8a
MSI KATANA <mohammed.mahmood@semaai.com>
2022-12-17 20:21:27 +0530

    #Android App Template 1) Data classes setup

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteDataSource/SomeRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.androidapplicationtemplate.data.remote.remoteDataSource
+
+import com.example.androidapplicationtemplate.data.remote.remoteServices.SomeService
+
+class SomeRemoteDataSource(
+	val someService: SomeService,
+) {
+
+}
