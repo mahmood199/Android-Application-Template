68cfdff8fcd63e97f27982e6730e2c2afcb7b4f4
MSI KATANA <mohammed.mahmood@semaai.com>
2022-12-17 20:14:13 +0530

    #Android App Template 1) Data classes setup

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/entity/Entity.kt
@@ -0,0 +1,5 @@
+package com.example.androidapplicationtemplate.data.models.entity
+
+data class Entity(
+	val x: Int,
+)
---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Response.kt
@@ -0,0 +1,5 @@
+package com.example.androidapplicationtemplate.data.models.response
+
+data class Response(
+	val x: Int,
+)
