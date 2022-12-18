c09b05c595c111dda7e076558e52a906c05c4cd3
MSI KATANA <mohammed.mahmood@semaai.com>
2022-12-18 13:34:13 +0530

    #Android App Template 1) restructuring

--- app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeActivity.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeActivity.kt
@@ -3,7 +3,9 @@ package com.example.androidapplicationtemplate.ui.someFeature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidapplicationtemplate.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SomeActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeViewModel.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.androidapplicationtemplate.ui.someFeature
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class SomeViewModel : ViewModel() {
 }
