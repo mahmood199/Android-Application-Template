0d9450ab0d061e4e8859c8e87e4b533687ada88d
MSI KATANA <mohammed.mahmood@semaai.com>
2022-12-18 13:33:19 +0530

    #Android App Template 1) restructuring

---
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidapplicationtemplate.ui.someFeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidapplicationtemplate.R
+
+class SomeActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_some)
+	}
+}
