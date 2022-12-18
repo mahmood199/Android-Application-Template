1c8aa1fa54a31f62ec90fc9c511d77c9afe4c2f3
MSI KATANA <mohammed.mahmood@semaai.com>
2022-12-18 13:23:55 +0530

    #Android App Template 1) Domain Classes setup

--- app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/SomeService.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteServices/SomeService.kt
@@ -1,4 +1,8 @@
 package com.example.androidapplicationtemplate.data.remote.remoteServices
 
+import com.example.androidapplicationtemplate.data.models.request.Request
+import com.example.androidapplicationtemplate.data.models.response.Response
+
 interface SomeService {
+	suspend fun loginUser(request : Request) : Response
 }
