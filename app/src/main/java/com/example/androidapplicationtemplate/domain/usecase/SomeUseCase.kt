1106e06c18ae7c5646d59a302448bb6ce7b67fb9
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-01 14:58:55 +0530

    Repository to return result object wrapped in Resource class including failures

--- app/src/main/java/com/example/androidapplicationtemplate/domain/usecase/SomeUseCase.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/domain/usecase/SomeUseCase.kt
@@ -1,9 +1,22 @@
 package com.example.androidapplicationtemplate.domain.usecase
 
+import com.example.androidapplicationtemplate.core.network.Resource
 import com.example.androidapplicationtemplate.domain.repository.SomeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class SomeUseCase @Inject constructor(
-	someRepository: SomeRepository,
+    private val someRepository: SomeRepository,
 ) {
+
+    operator fun invoke(): Flow<Resource<String>> = flow {
+        emit(Resource.Loading)
+        val result = someRepository.someCrudOperation()
+        emit(result)
+    }.flowOn(Dispatchers.IO)
+
+
 }
