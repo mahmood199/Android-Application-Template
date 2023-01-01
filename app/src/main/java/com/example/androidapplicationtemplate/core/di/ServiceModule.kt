790e55eef45e8d4dc68c3a815b9b9fa6192b6904
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-01-01 15:05:22 +0530

    Revert "Feat/sample"

--- app/src/main/java/com/example/androidapplicationtemplate/core/di/ServiceModule.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/core/di/ServiceModule.kt
@@ -2,7 +2,6 @@ package com.example.androidapplicationtemplate.core.di
 
 import com.example.androidapplicationtemplate.core.network.RetrofitClient
 import com.example.androidapplicationtemplate.data.remote.remoteServices.SomeService
-import com.example.androidapplicationtemplate.sample.data.PostService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,11 +15,5 @@ object ServiceModule {
 	fun provideSomeService(): SomeService {
 		return RetrofitClient.getClient().create(SomeService::class.java)
 	}
-
-	@Provides
-	fun providePostService(): PostService {
-		return RetrofitClient.getClient().create(PostService::class.java)
-	}
-
 
 }
--- app/src/main/java/com/example/androidapplicationtemplate/sample/domain/PostRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidapplicationtemplate.sample.domain
-
-import com.example.androidapplicationtemplate.sample.data.PostEntity
-
-interface PostRepository {
-	suspend fun getPosts(): Result<ArrayList<PostEntity>>
-}
