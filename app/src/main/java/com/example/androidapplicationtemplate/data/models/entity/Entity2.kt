2860784ea63eb4942727e5841bc8f149a1babaef
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-01 23:45:49 +0530

    Sample Model classes added
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/entity/Entity2.kt
@@ -0,0 +1,7 @@
+package com.example.androidapplicationtemplate.data.models.entity
+
+import com.example.androidapplicationtemplate.core.platform.AdapterItem
+
+data class Entity2(
+    val someValue : String
+) : AdapterItem
