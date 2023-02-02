7600fbef10108c62594203ddf5578e38cf2c0150
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-02 21:11:39 +0530

    list item added to Rview
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeActivity.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/SomeActivity.kt
@@ -2,9 +2,15 @@ package com.example.androidapplicationtemplate.ui.someFeature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.androidapplicationtemplate.R
+import com.example.androidapplicationtemplate.core.platform.AdapterItem
+import com.example.androidapplicationtemplate.core.platform.GenericAdapter
+import com.example.androidapplicationtemplate.core.platform.RecyclerViewItemClickAction
+import com.example.androidapplicationtemplate.data.models.entity.Entity1
+import com.example.androidapplicationtemplate.data.models.entity.Entity2
 import com.example.androidapplicationtemplate.databinding.ActivitySomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -18,11 +24,52 @@ class SomeActivity : AppCompatActivity() {
 	private val binding by lazy {
 		ActivitySomeBinding.inflate(layoutInflater)
 	}
+	private val adapter = GenericAdapter {
+		handleClickListener(it)
+	}
+
+	private fun handleClickListener(it: RecyclerViewItemClickAction) {
+		when(it) {
+			is RecyclerViewItemClickAction.ClickInterceptorOne -> {
+				Toast.makeText(this, "${it.viewType} clicked", Toast.LENGTH_SHORT).show()
+			}
+			is RecyclerViewItemClickAction.ClickInterceptorTwo -> {
+				Toast.makeText(this, "${it.viewType} clicked", Toast.LENGTH_SHORT).show()
+			}
+		}
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 		setObservers()
+		addItemsToList()
+	}
+
+	private fun addItemsToList() {
+		val list = mutableListOf<AdapterItem>()
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		list.add(Entity1("View 1"))
+		list.add(Entity2("View 2"))
+		adapter.addItems(list)
+		binding.apply {
+			rvSome.adapter = adapter
+		}
 	}
 
 	private fun setObservers() {
