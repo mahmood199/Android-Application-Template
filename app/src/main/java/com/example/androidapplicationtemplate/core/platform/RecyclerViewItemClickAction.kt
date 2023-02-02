fd8b6d60c247f3575ba741a86b9f77c5de27d6e6
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-02 21:11:24 +0530

    Click item added to RecyclerViewItemClickAction.kt
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/core/platform/RecyclerViewItemClickAction.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/core/platform/RecyclerViewItemClickAction.kt
@@ -1,5 +1,6 @@
 package com.example.androidapplicationtemplate.core.platform
 
 sealed class RecyclerViewItemClickAction {
-    data class ClickInterceptorOne(val position: Int) : RecyclerViewItemClickAction()
+    data class ClickInterceptorOne(val viewType: Int) : RecyclerViewItemClickAction()
+    data class ClickInterceptorTwo(val viewType: Int) : RecyclerViewItemClickAction()
 }
